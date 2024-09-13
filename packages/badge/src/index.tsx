64a7016ef8270e479d35700891c254d6c2e5b28b
Bashakin <bashakin@greensight.ru>
2024-09-13 12:27:05 +0300

    ensitech-154 updated badge

--- packages/badge/src/index.tsx
+++ packages/badge/src/index.tsx
@@ -1,50 +1,37 @@
-import { CSSObject } from '@emotion/react';
 import { STATUSES, defaultTheme, scale } from '@greensight/core-components-common';
 
 const { colors, typography } = defaultTheme;
 
 export interface BadgeProps {
     text: string;
+    bgColor?: string;
     type?: STATUSES;
 }
 
-export type StatusColors = Record<STATUSES, CSSObject>;
-
-const defaultStatusColors: StatusColors = {
-    created: {
-        color: colors.white,
-        backgroundColor: colors.primary,
-    },
-    success: {
-        color: colors.white,
-        backgroundColor: colors.success,
-    },
-    error: {
-        color: colors.white,
-        backgroundColor: colors.danger,
-    },
-    warning: {
-        color: colors.white,
-        backgroundColor: colors.warning,
-    },
-    regular: {
-        color: colors.white,
-        backgroundColor: colors.secondaryHover,
-    },
+const STATUS_COLORS: Record<STATUSES, string> = {
+    [STATUSES.CREATED]: colors?.primary,
+    [STATUSES.SUCCESS]: colors?.success,
+    [STATUSES.ERROR]: colors?.danger,
+    [STATUSES.WARNING]: colors?.warning,
+    [STATUSES.REGULAR]: colors?.secondaryHover,
 };
 
-export const Badge = ({ text, type = STATUSES.REGULAR }: BadgeProps) => (
-    <div
-        css={{
-            display: 'inline-flex',
-            alignItems: 'center',
-            ...defaultStatusColors[type],
-            borderRadius: 2,
-            padding: `1px ${scale(1, true)}px`,
-            whiteSpace: 'pre-line',
-            ...typography('smallBold'),
-        }}
-    >
-        {text}
-    </div>
-);
+export const Badge = ({ text, bgColor = colors.secondaryHover, type = STATUSES.REGULAR }: BadgeProps) => {
+    const backgroundColor = STATUS_COLORS[type] || bgColor;
+
+    return (
+        <div
+            css={{
+                display: 'inline-flex',
+                alignItems: 'center',
+                backgroundColor,
+                borderRadius: 2,
+                padding: `1px ${scale(1, true)}px`,
+                whiteSpace: 'pre-line',
+                ...typography('smallBold'),
+            }}
+        >
+            {text}
+        </div>
+    );
+};
