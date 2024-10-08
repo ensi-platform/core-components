0a36bdb35bbbe108d9d80b887c26596cdef15713
mskhelen <moskvitina@greensight.ru>
2024-10-08 14:49:46 +0300

    ensitech-85 refactoring Block

--- packages/block/src/index.tsx
+++ packages/block/src/index.tsx
@@ -1,5 +1,5 @@
 import { jsx } from '@emotion/react';
-import { ElementType, ReactNode } from 'react';
+import { ElementType } from 'react';
 
 import { scale, defaultTokens } from '@greensight/core-components-common';
 
@@ -8,21 +8,19 @@ import BlockMobileFluid from './components/BlockBodyFluid';
 import BlockFooter from './components/BlockFooter';
 import BlockHeader from './components/BlockHeader';
 
-type BlockProps<P extends ElementType = 'section'> = {
-    /** Use your own React component for render. */
-    as?: P;
-    children: ReactNode | ReactNode[];
-    className?: string;
-    background?: string;
-    boxShadow?: string;
-};
+import { IBlockProps } from './types';
+
+export { BlockHeader };
+export { BlockMobileFluid };
+export { BlockBody };
+export { BlockFooter };
 
 export const Block = <T extends ElementType = 'section'>({
     as,
     background = defaultTokens.colors.white,
     boxShadow = defaultTokens.shadows.big,
     ...props
-}: BlockProps<T>) =>
+}: IBlockProps<T>) =>
     jsx(as || 'section', {
         css: { boxShadow, width: '100%', backgroundColor: background, borderRadius: scale(1) },
         ...props,
