7151668268770c93e71496843e32622db0beafd6
Kseniya Ryasnyanskaya <ryasnyanskaya_kseniya@inbox.ru>
2024-10-18 19:05:04 +0300

    ENSITECH-245 fix popover.stories

--- packages/popover/README.md
+++ packages/popover/README.md
@@ -1,4 +1,7 @@
-# Компонент popover
-Компонент Popover. Под капотом использует [popper.js](popper.js.org) и [react-transition-state](https://github.com/szhsin/react-transition-state).
+# Popover component
 
-Основан на  [alfalab/core-components](https://github.com/alfa-laboratory/core-components)
+The Popover component is a versatile UI element that displays content relative to an anchor element. It leverages the power of [popper.js](popper.js.org).js for positioning and [react-transition-state](https://github.com/szhsin/react-transition-state) for smooth transitions.
+
+This component is designed to show additional information or interactive elements like tooltips or menus in a floating layer near the anchor element. It can be customized for various display options such as position, transitions, and size.
+
+Based on [alfalab/core-components](https://github.com/alfa-laboratory/core-components)
--- packages/popover/src/popover.stories.tsx
+++ packages/popover/src/popover.stories.tsx
@@ -3,6 +3,7 @@ import { useState, ComponentProps, useRef } from 'react';
 
 import README from '../README.md';
 import { Popover } from './index';
+import { POSITION_OPTIONS } from './scripts'
 
 export default {
     title: 'Components / Popover',
@@ -14,17 +15,69 @@ export default {
             },
         },
     },
+    args: {},
+    argTypes: {},
 } as Meta<typeof Popover>;
 
 export const Basic: StoryObj<Omit<ComponentProps<typeof Popover>, 'anchorElement' | 'open'>> = {
     args: {
         withTransition: true,
+        withArrow: false,
+        position: "right",
+        preventFlip: false,
+        preventOverflow: false,
+        availableHeight: false,
+        useAnchorWidth: false,
+        offset: [0, 0],
+        zIndex: 1000,
+        popperCSS: {},
+        arrowCSS: {},
     },
-    parameters: {
-        docs: {
-            description: {
-                story: 'Обычное применение',
-            },
+    argTypes: {
+        withTransition: {
+            description: "Enables or disables the animation when opening or closing the popover. If set to true, an animation will be applied during the popover's appearance and disappearance.",
+            control: { type: 'boolean' },
+        },
+        withArrow: {
+            description: "Determines whether an arrow should be displayed on the popover, pointing to the anchor element. When set to true, the arrow will be shown.",
+            control: { type: 'boolean' },
+        },
+        position: {
+            description: "Controls the position of the popover relative to the anchor element (anchorElement). The available values include \"top\", \"right\", \"bottom\", and \"left\". In this example, the default is \"right\".",
+            options: POSITION_OPTIONS,
+            control: { type: 'select' },
+        },
+        preventFlip: {
+            description: "Prevents the popover from changing its position if there is not enough space in the specified direction. When set to true, the popover will remain in its specified position.",
+            control: { type: 'boolean' },
+        },
+        preventOverflow: {
+            description: "Ensures that the popover stays within the boundaries of the visible screen or container, preventing it from overflowing. If true, the popover will not extend outside the visible area.",
+            control: { type: 'boolean' },
+        },
+        availableHeight: {
+            description: "Adjusts the popover’s height based on available screen space. When true, the popover will adapt its height to fit within the screen.",
+            control: { type: 'boolean' },
+        },
+        useAnchorWidth: {
+            description: "Uses the width of the anchor element for the popover. If true, the popover’s width will match the width of the anchor element.",
+            control: { type: 'boolean' },
+        },
+        offset: {
+            description: "Sets the offset of the popover relative to the anchor element. For example, if the position is \"right\", the offset could shift the popover horizontally or vertically.",
+            control: { type: 'object' },
+        },
+        zIndex: {
+            description: "Controls the z-index of the popover, determining its stacking order relative to other elements on the page. The default value in this example is 1000.",
+            control: { type: 'number' },
+        },
+        popperCSS: {
+            description: "Allows additional custom styles to be applied to the popover itself. You can pass any CSS properties as part of this object to customize the appearance.",
+            control: { type: 'object' },
+        },
+        arrowCSS: {
+            description: "Allows custom styles to be applied to the arrow. Like popperCSS, this property accepts CSS properties to style the arrow's appearance.",
+            control: { type: 'object' },
         },
     },
     render: args => {
